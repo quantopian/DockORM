366d973633ebeebbeda472336473c4638e4ab40e
Scott Sanderson <scoutoss@gmail.com>
2014-11-17 03:57:51 -0500

    TEST: Cleanup images before/after tests and add decoy container.

--- dockorm/tests/conftest.py
+++ dockorm/tests/conftest.py
@@ -22,14 +22,17 @@ def test_container(image, **kwargs):
 
 
 @fixture(scope='session', autouse=True)
-def clean_test_images():
+def clean_test_images(request):
     """
-    Automatically clear all test images at the end of a test run.
+    Automatically clear all test images after each test run.
     """
-    client = Client()
-    test_images = client.images(TEST_ORG + "/*")
-    for image in test_images:
-        client.remove_image(image)
+    def cleanup():
+        client = Client()
+        test_images = client.images(TEST_ORG + "/*")
+        for image in test_images:
+            client.remove_image(image)
+    cleanup()
+    request.addfinalizer(cleanup)
 
 
 @fixture
@@ -45,3 +48,23 @@ def busybox(request):
     request.addfinalizer(clean)
 
     return bb
+
+
+@fixture(autouse=True)
+def decoy(request):
+    """
+    An extra copy of the busybox container to run during all tests.
+
+    Ensures that by-name filters correctly ignore differently-named containers.
+    """
+    bb = test_container('busybox_decoy')
+    if not bb.images():
+        bb.build(display=False)
+
+    def clean():
+        bb.purge(stop_first=False, remove_volumes=True)
+
+    request.addfinalizer(clean)
+
+    bb.run(['sleep', '2147483647'])
+    assert bb.running()
